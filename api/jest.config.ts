a298692109ba7238d8dee8c00890d4fd1737bf57
Kashish Thesia <kthesia93@gmail.com>
2025-09-08 07:34:32 +0200

    fix: flatten monorepo (move COS/careeros → repo root)

--- api/jest.config.ts
+++ api/jest.config.ts
@@ -1,3 +1,5 @@
+const { pathsToModuleNameMapper } = require('ts-jest');
+const { compilerOptions } = require('../../tsconfig.base.json');
 export default {
   displayName: 'api',
   preset: '../jest.preset.js',

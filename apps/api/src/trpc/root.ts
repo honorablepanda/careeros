9747a6b8a247aea49470cda3127be3ff0299a979
Kashish Thesia <kthesia93@gmail.com>
2025-09-12 17:14:02 +0200

    feat(api): solid applications router (test-compatible inputs); wire into TRPC root

--- apps/api/src/trpc/root.ts
+++ apps/api/src/trpc/root.ts
@@ -1,8 +1,8 @@
 import { summaryRouter } from '../router/summary';
 import { router } from './trpc';
 import { trackerRouter } from './routers/tracker.router';
+import { applicationsRouter } from './routers/applications.router';
 export const appRouter = router({ 
-  summary: summaryRouter,
-tracker: trackerRouter });
+  applications: applicationsRouter, tracker: trackerRouter });
 export type AppRouter = typeof appRouter;
 export { createContext } from './context';

df401ee8651cd9afb02aec4b00cf7e82c1c69ef7
Kashish Thesia <kthesia93@gmail.com>
2025-09-16 16:08:41 +0200

    chore(web): remove old archived web copy; canonical app lives at ./web

--- apps/web._archived_2025-09-15T18-56-10-787Z/.app_backup_20250914_133957/page.tsx
+++
@@ -1,8 +0,0 @@
-export default function Home() {
-  return (
-    <main style={{ padding: 24 }}>
-      <h1>CareerOS</h1>
-      <p>Try: <code>/tracker/activity?id=&lt;application-id&gt;</code> or <code>/tracker/[id]/activity</code>.</p>
-    </main>
-  );
-}
--- apps/web._archived_2025-09-15T18-56-10-787Z/src/app/providers.tsx
+++
@@ -1,6 +0,0 @@
-/* eslint-disable react-refresh/only-export-components */
-'use client';
-import React from 'react';
-export function Providers({ children }: { children: React.ReactNode }) {
-  return <>{children}</>;
-}
